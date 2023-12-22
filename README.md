dfb55e3dcb2a9745bfb646c50c399d7f44f96c33
miminashii <koqchannotifier@gmail.com>
2023-12-22 23:42:18 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # twitch-alarm
-とめる/koQchanが配信開始したらアラーム鳴らす
+
+とめる/koQchanが配信開始したらアラーム鳴らす。\
+実行をctrl+cとかで止めない限り永遠と動く。
+
+## 使う上での注意点
+
+- Mac端末上でしか動作確認していません（Windowsだと動くかわからないです）
+
+## 使い方
+
+1. Python 3.11をインストール
+2. 必要なパッケージをインストール
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+3. 実行
+
+   ```bash
+   python twitch_alarm.py
+   ```
+
+## FYI
+
+- `config.yaml`の`tomeru1`、`koqchan`を任意のチャンネルに変更すれば、そのチャンネルの配信開始を検知してアラームを鳴らせる。
+- `config.yaml`の`alarm_filepath`を変更することで、好きなアラームの音を鳴らせる。
---
+++ twitch_alarm.py
@@ -0,0 +1,106 @@
+import asyncio
+import sys
+from dataclasses import dataclass, field
+from typing import List
+
+import yt_dlp
+from pydub import AudioSegment
+from pydub.playback import play
+from yaml import safe_load
+
+TWITCH_BASE_URL = "https://www.twitch.tv"
+
+
+def main():
+    try:
+        stored_config = _read_config()
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(_check_loop(stored_config))
+    except KeyboardInterrupt:
+        print("\nInterrupted by user.")
+        sys.exit(1)
+
+
+@dataclass
+class Config:
+    twitch_watched_channels: List[str]
+    interval_seconds: int = field(default=60)
+    alarm_filepath: str = field(default="res/ドラえもんの目覚まし時計の歌 covered by 息根とめる.wav")
+
+    def __init__(self, local_config: dict):
+        self.twitch_watched_channels = local_config["twitch"]["watched_channels"]
+
+        if "interval_seconds" in local_config.keys():
+            if local_config["interval_seconds"]:
+                self.interval_seconds = local_config["interval_seconds"]
+
+        if "alarm_filepath" in local_config.keys():
+            if local_config["alarm_filepath"]:
+                self.alarm_filepath = local_config["alarm_filepath"]
+
+
+def _read_config():
+    with open("config.yaml", "r", encoding="utf-8") as s:
+        return Config(safe_load(s))
+
+
+async def _check_loop(config: Config):
+    tasks = []
+    for channel in config.twitch_watched_channels:
+        tasks.append(_check_loop_child(channel, config))
+    await asyncio.gather(*tasks)
+
+
+async def _check_loop_child(channel: str, config: Config):
+    print(f'Waiting for "{channel}" to start a stream...')
+    currently_streaming = False
+    while True:
+        try:
+            if (is_live := _is_live(channel)) and not currently_streaming:
+                print(f'The channel "{channel}" started a stream!')
+                currently_streaming = True
+                _alarm(config)
+            elif is_live and currently_streaming:
+                pass
+            else:
+                currently_streaming = False
+            await asyncio.sleep(config.interval_seconds)
+        except Exception as err:
+            print(err)
+            await asyncio.sleep(config.interval_seconds)
+
+
+# 配信開始してない時に出る邪魔なエラーログを出したくなくて定義している
+class LoggerOutputs:
+    def error(msg):
+        pass
+
+    def warning(msg):
+        pass
+
+    def debug(msg):
+        pass
+
+
+def _is_live(channel: str, ydl_opts: dict = {"quiet": True}):
+    ydl_opts["logger"] = LoggerOutputs
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.extract_info(_get_url(channel), download=False)
+        return True
+    except yt_dlp.utils.DownloadError:
+        return False
+
+
+def _alarm(config: Config):
+    sound = AudioSegment.from_wav(config.alarm_filepath)
+    play(sound)
+
+
+def _get_url(channel: str):
+    return f"{TWITCH_BASE_URL}/{channel}"
+
+
+if __name__ == "__main__":
+    exit(main())
